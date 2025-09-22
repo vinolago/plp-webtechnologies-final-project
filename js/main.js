f9f993c3f92b0ba012b4611636a331da8961d1be
vinolago <dr.plancksolago@gmail.com>
2025-09-22 22:41:21 +0300

    delete py tests

--- js/main.js
+++ js/main.js
@@ -32,5 +32,13 @@ accordions.forEach(header => {
   });
 });
 
+// Testimonials carousel
+const testimonials = document.querySelectorAll('.testimonial');
+let index = 0;
+setInterval(() => {
+  testimonials[index].classList.remove('active');
+  index = (index + 1) % testimonials.length;
+  testimonials[index].classList.add('active');
+}, 4000);
 
 
